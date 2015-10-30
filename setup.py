839bbdf0635c3f02b6ca6ed4aec6ba456b2f67bf
SGW <sgwoodjr@gmail.com>
2015-10-29 22:21:42 -0400

    Add setup.py and a travis configuration file

---
+++ setup.py
@@ -0,0 +1,116 @@
+
+#------------------------------------------------------------------------
+# Copyright (c) 2015 SGW
+#
+# Distributed under the terms of the New BSD License.
+#
+# The full License is in the file LICENSE
+#------------------------------------------------------------------------
+
+# Use setuptools to recognize the install_requires option.
+from setuptools import setup
+
+import os
+import subprocess
+
+MAJOR = 0
+MINOR = 1
+MICRO = 0
+ISRELEASED = False
+VERSION = '%d.%d.%d' % (MAJOR,MINOR,MICRO)
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+def get_version_info():
+    # Adding the git rev number needs to be done inside
+    # write_version_py(), otherwise the import of scipy.version messes
+    # up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('velvet/version.py'):
+        # must be a source distribution, use existing version file
+        # load it as a separate module to not load velvet/__init__.py
+        import imp
+        version = imp.load_source('velvet.version', 'velvet/version.py')
+        GIT_REVISION = version.git_revision
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev-' + GIT_REVISION[:7]
+
+    return FULLVERSION, GIT_REVISION
+
+
+def write_version_py(filename='velvet/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM VELVET SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+"""
+    FULLVERSION, GIT_REVISION = get_version_info()
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version' : FULLVERSION,
+                       'git_revision' : GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+def setup_package():
+
+    # Rewrite the version file everytime
+    write_version_py()
+
+    setup(name='velvet',
+        version=VERSION,
+        author='SGW',
+        author_email='sgwoodjr@gmail.com',
+        url='https://github.com/sgwoodjr/velvet',
+        packages=['velvet','velvet.test'],
+        license = 'LICENSE',
+        description='Beautiful signal processing and communications for Python',
+        long_description=open('README.md').read(),
+        install_requires=[
+            "python >= 2.7",
+            "NumPY >= 1.7",
+            "SciPY >= 0.13",
+            "matplotlib >= 1.3.1",
+        ],
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
