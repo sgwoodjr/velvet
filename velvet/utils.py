78f61e0952a767e64fe0f0ad23515f0c55e499b1
SGW <sgwoodjr@gmail.com>
2015-11-10 20:55:57 -0500

    Add utils.ProgressBar
    ProgressBar is a utility function that animates the running progress of
    a computation. The code is lifted from an IPython notebook example.

--- velvet/utils.py
+++ velvet/utils.py
@@ -9,10 +9,11 @@
 # The full License is in the file LICENSE
 #------------------------------------------------------------------------
 
+import sys
 import numpy as np
 
 # Public API
-__all__ = ['isodd']
+__all__ = ['isodd', 'ProgressBar']
 
 
 
@@ -53,3 +54,43 @@ def isodd(x):
 
 
     return x & 1
+
+class ProgressBar:
+    """Print a progress bar to the terminal
+
+    Code grabbed from an Ipython Notebook example.
+
+    Example useage:
+    P = ProgressBar(10)
+    for ii in arange(10):
+        p.animate(ii+1)
+    """
+
+    def __init__(self, iterations):
+        self.iterations = iterations
+        self.prog_bar = '[]'
+        self.fill_char = '*'
+        self.width = 50
+        self.__update_amount(0)
+
+    def animate(self, iter):
+        print '\r', self,
+        sys.stdout.flush()
+        self.update_iteration(iter + 1)
+
+    def update_iteration(self, elapsed_iter):
+        self.__update_amount((elapsed_iter / float(self.iterations)) * 100.0)
+        self.prog_bar += '  %d of %s complete' % (elapsed_iter, self.iterations)
+
+    def __update_amount(self, new_amount):
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
+        pct_place = (len(self.prog_bar) // 2) - len(str(percent_done))
+        pct_string = '%d%%' % percent_done
+        self.prog_bar = self.prog_bar[0:pct_place] + \
+            (pct_string + self.prog_bar[pct_place + len(pct_string):])
+
+    def __str__(self):
+        return str(self.prog_bar)
