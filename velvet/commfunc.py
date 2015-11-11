35c7970bdff3b6bf398ae58f1f5a36358c963ad9
SGW <sgwoodjr@gmail.com>
2015-11-10 21:49:23 -0500

    Add berawgn(...) function
    Add a function to compute the theoretical bit-error-rate in an additive
    white Gaussian noise channel (AWGN). The function currently supports
    CPFSK signals.

--- velvet/commfunc.py
+++ velvet/commfunc.py
@@ -16,7 +16,94 @@ from .validation import assert_ndarray, assert_one_dimension
 erfc = scipy.special.erfc
 
 # Public API
-__all__ = ['biterr', 'qfunc'] 
+__all__ = ['berawgn', 'biterr', 'qfunc'] 
+
+def berawgn(EbNodB, mod_type, M, mod_index=None, Kmin=None):
+    """
+    Bit error rate (BER) for additive white Gaussian noise channels
+
+    ber = berawgn(EbNodB, mod_type, M) returns the theoretical BER of various
+    modulation types over an additive white Gaussian noise channel. The
+    input, EbNodb, is the bit energy to noise power spectral density
+    ratio in dB. mod_type corresponds to the modulation type and M
+    is the modulation alphabet size. 
+
+    ber = berawgn(EbNodB, 'cpfsk', M, mod_index, Kmin) returns a lower
+    bound on the BER for Continuous Phase Frequency Shift Keying (CPFSK).
+    mod_index and Kmin specifiy the modulation index and the number
+    of paths having the minimum distance, respectively. If the number
+    of minimum distance paths is unknown, then a value of Kmin = 1
+    is appropriate.
+    
+    The supported modulations are listed in the following table.
+
+    =====================  ==========   ============    ==========
+    Modulation Scheme      mod_type     Range of M      Encoding
+    =====================  ==========   ============    ==========
+    CPFSK                  'cpfsk'      Power of 2      N/A
+    =====================  ==========   ============    ==========
+
+
+    Parameters
+    -----------
+    EbNodB : {scalar, ndarray}
+             Ratio of bit energy to noise power spectral density, in dB
+        
+    mod_type : string
+              Modulation type. Refer to table for accepted values
+
+    M : int
+        Modulation alphabet size
+
+    mod_index : float
+        Modulation index for CPFSK
+
+    Kmin : int
+        Number of paths having the minimum distance
+
+
+    Returns
+    -------
+    ber : {scalar, ndarray}
+          The theoretical bit error rate
+
+    References
+    -----------
+    .. [1] Proakis, J.G., Digital Communications, 4th ed., McGraw-Hill, 2001.
+
+    Examples
+    ---------
+    >>> import velvet as vt
+    >>> import matplotlib.pyplot as plt
+    >>> import numpy as np
+
+    >>> EbNodB = np.arange(0,16.)
+    >>> ber_cpfsk = kpi.berawgn(EbNodB,'cpfsk', 4, 0.5, 1)
+    >>> plt.semilogy(EbNodB,ber_cpfsk);
+    >>> plt.grid('on')
+    >>> plt.xlabel(r'$\\frac{E_b}{N_0}$')
+    >>> plt.ylabel(r'$P_e$')
+    >>> plt.legend(('4-CPFSK'))
+    >>> plt.show()
+
+    """
+
+    # Convert from dB to linear scale
+    EbNoLin = 10**(EbNodB/10.0)
+
+    mod_type = str.lower(mod_type)
+
+    if mod_type == 'cpfsk':
+        if mod_index is None or Kmin is None:
+            raise ValueError("Values for mod_index and Kmin are required")
+        k = np.arange(1, M)
+        db2 = min((2.0 * np.log2(M)) * (1 - np.sinc(2*k*mod_index)))
+        Pm = Kmin * qfunc(np.sqrt(EbNoLin * db2))
+        ber = Pm / np.log2(M)
+    else:
+        raise ValueError("Modulation type must bet one of {'cpfsk'}")
+      
+    return ber
 
 
 def biterr(x, y):
--- velvet/test/test_commfunc.py
+++ velvet/test/test_commfunc.py
@@ -14,6 +14,25 @@ import velvet as vt
 
 class TestCommFunctions(unittest.TestCase):
 
+    def test_berawgn_cpfsk(self):
+        EbNodB = np.arange(10)
+        ber = vt.berawgn(EbNodB,'cpfsk', 2, 0.5, 1)
+        berC = np.array([  7.86496035e-02,   5.62819520e-02,   3.75061284e-02,
+            2.28784076e-02,   1.25008180e-02,   5.95386715e-03,
+            2.38829078e-03,   7.72674815e-04,   1.90907774e-04,
+            3.36272284e-05])
+        for ind in np.arange(len(ber)):
+            self.assertAlmostEqual(ber[ind],berC[ind])
+
+    def test_berawgn_cpfsk_ValueError(self):
+        EbNodB = np.arange(10)
+        self.assertRaises(ValueError, vt.berawgn, EbNodB, 'cpfsk', 2)
+
+    def test_berawgn_modtype_ValueError(self):
+        EbNodB = np.arange(10)
+        unsupported_mod_type = 'qam'
+        self.assertRaises(ValueError, vt.berawgn, EbNodB, unsupported_mod_type, 2)
+
     def test_biterr(self):
         x = np.array([0, 1, 1, 0])
         y = np.array([1, 1, 0, 0])
